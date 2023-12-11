d38d29bbe562dea8bbbb926576cbf7c15b152d7d
Hanraf <raihanrafi96@gmail.com>
2023-12-11 12:04:34 +0700

    Add Sorensen-Dice Coefficient

--- backend/app/Controllers/Api.php
+++ backend/app/Controllers/Api.php
@@ -9,6 +9,7 @@ use App\Models\PernyataanSiswaModel;
 use App\Models\PernyataanModel;
 use App\Models\KasusModel;
 use CodeIgniter\API\ResponseTrait;
+use App\Helpers\hitungan;
 
 class Api extends BaseController
 {
@@ -16,21 +17,52 @@ class Api extends BaseController
 
     public function index()
     {
+        // Deklarasi models
         $basecmodel = new BaseCaseModel();
         $kasusmodel = new KasusModel();
         $siswamodel = new SiswaModel();
         $pernyataanmodel = new PernyataanModel();
         $psiswamodel = new PernyataanSiswaModel();
 
-        // Set value data yang dikirim untuk reply / respond
-        $data = [
-            'base case' => $basecmodel->GetCaseBase(),
-            'kasus' => $kasusmodel->GetKasus(),
-            'siswa' => $siswamodel->GetSiswa(),
-            'pernyataan' => $pernyataanmodel->GetPernyataan(),
-            'pernyataan siswa' => $psiswamodel->GetPernyataanSiswa()
-        ];
+        // Set value data yang dikirim untuk reply / 
+        // Menentukan query parameter yang diterima
+        $params = $this->request->getGet();
 
+        // Set value data yang dikirim untuk reply / respond
+        $data = [];
+
+        // Memeriksa setiap parameter dan memuat data yang sesuai
+        if (empty($params)){
+            $data = [
+                'base_case' => $basecmodel->GetCaseBasePernyataan(),
+                'kasus' => $kasusmodel->GetKasus(),
+                'siswa' => $siswamodel->GetSiswa(),
+                'pernyataan' => $pernyataanmodel->GetPernyataan(),
+                'pernyataan siswa' => $psiswamodel->GetPernyataanSiswa()
+            ];
+        } else {
+            foreach ($params as $param => $value) {
+                switch ($param) {
+                    case 'base_case':
+                        $data['base_case'] = $basecmodel->GetCaseBasePernyataan();
+                        break;
+                    case 'kasus':
+                        $data['kasus'] = $kasusmodel->GetKasus();
+                        break;
+                    case 'siswa':
+                        $data['siswa'] = $siswamodel->GetSiswa();
+                        break;
+                    case 'pernyataan':
+                        $data['pernyataan'] = $pernyataanmodel->GetPernyataan();
+                        break;
+                    case 'pernyataan_siswa':
+                        $data['pernyataan_siswa'] = $psiswamodel->GetPernyataanSiswa();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
         // Respon ke Request
         if ($data) {
             return $this->respond($data);
@@ -41,7 +73,7 @@ class Api extends BaseController
 
     public function create()
     {
-        // Ambil data siswa dari form
+        // Ambil input dari form
         $data_siswa['umur'] = $this->request->getJSON()->umur;
         $data_siswa['jenis_kelamin'] = $this->request->getJSON()->jenis_kelamin;
         $data_siswa['kelas'] = $this->request->getJSON()->kelas;
@@ -51,6 +83,7 @@ class Api extends BaseController
         $siswaModel->insertData($data_siswa);
         $id_siswa = $siswaModel->getInsertID(); // Ambil ID siswa yang baru saja dimasukkan ke tabel `siswa`
         
+        // Tambah kasus baru ke tabel 'kasus'
         $kasusModel = new KasusModel();
         $data_kasus['id_siswa'] = $id_siswa;
         $kasusModel->insertData($data_kasus);
@@ -63,12 +96,24 @@ class Api extends BaseController
         $pernyataanSiswaModel->insertData($data_pernyataansiswa);
         //$id_pernyataansiswa = $pernyataanSiswaModel->getInsertID();
 
+        $basecmodel = new BaseCaseModel();
+        $baseCase = $basecmodel->GetCaseBasePernyataan();
+
+        $sdc = sdc($data_pernyataansiswa['id_pernyataan'], $baseCase);
+
+        // Set data Respond
         $data = [
             'msg' => 'Berhasil~!',
             'id_siswa' => $id_siswa,
             'id_kasus' => $id_kasus,
+            'umur' => $data_siswa['umur'],
+            'jenis_kelamin' => $data_siswa['jenis_kelamin'],
+            'kelas' => $data_siswa['kelas'],
+            'hasil_sdc' => $sdc,
         ];
-        
+
+        $kasusModel->where('id_kasus', $id_kasus)->set('id_minatbakat', $sdc[0]['id_minatbakat'])->update();
+
         // Respon ke Request
         if ($data) {
             return $this->respond($data);
--- backend/app/Models/BaseCaseModel.php
+++ backend/app/Models/BaseCaseModel.php
@@ -54,6 +54,26 @@ class BaseCaseModel extends Model
                                 ->getResultArray();
     }
 
+    function GetCaseBasePernyataan(){
+        $result = $this->builder    ->select('base_case.*, siswa.*, kasus.*, minat_bakat.*, GROUP_CONCAT(pernyataan_siswa.id_pernyataan) AS id_pernyataan')
+                                    ->join('kasus', 'kasus.id_kasus = base_case.id_kasus')
+                                    ->join('siswa', 'kasus.id_siswa = siswa.id_siswa')
+                                    ->join('pernyataan_siswa', 'pernyataan_siswa.id_kasus = kasus.id_kasus')
+                                    ->join('pernyataan', 'pernyataan.id_pernyataan = pernyataan_siswa.id_pernyataan')
+                                    ->join('minat_bakat', 'minat_bakat.id_minatbakat = kasus.id_minatbakat')
+                                    ->groupBy('kasus.id_kasus')
+                                    ->get()
+                                    ->getResultArray();
+        
+        // Memisahkan hasil dari GROUP_CONCAT menjadi array
+        foreach ( $result as $result ){
+            $result['id_pernyataan'] = explode(',', $result['id_pernyataan']);
+            $cases[] = $result;
+        }
+
+        return $cases;
+    }
+
     function CekId($id){
         $result = $this->builder    ->where('kasus.id_basecase', $id)
                                     ->get()
